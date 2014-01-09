c391a3a410653c3bf00f3183cfb005fecd6bdf95
Heavy54 <mathieu.jo@sfr.fr>
2014-01-09 16:41:28 +0100

    Translation of image to keep it on Window change. !!!! Don't work for translation along y !!!!

--- src/Distortion/HomographyCalculator.cpp
+++ src/Distortion/HomographyCalculator.cpp
@@ -35,8 +35,8 @@ void HomographyCalculator::determineHomographies(vector<Plane> a, vector<Plane> 
 
         Plane p1 = a.at(i);
         Plane p2 = b.at(i);
-        vector<Point2f> src;
-        vector<Point2f> dst;
+        vector<Point2d> src;
+        vector<Point2d> dst;
 
         src.push_back(p1.getPoint(0));
         src.push_back(p1.getPoint(1));
@@ -48,9 +48,91 @@ void HomographyCalculator::determineHomographies(vector<Plane> a, vector<Plane> 
         dst.push_back(p2.getPoint(2));
         dst.push_back(p2.getPoint(3));
 
-        Mat m = findHomography(src, dst);
+        Mat H = findHomography(src, dst, CV_RANSAC);
 
-        homographies.push_back(m);
+        // Find the translation to keep image in the window
+        Mat P1 = Mat(3,1,CV_64F);
+        P1.at<double>(0,0) = p1.getPoint(0).x;
+        P1.at<double>(1,0) = p1.getPoint(0).y;
+        P1.at<double>(2,0) = 1;
+
+        Mat P2 = Mat(3,1,CV_64F);
+        P2.at<double>(0,0) = p1.getPoint(1).x;
+        P2.at<double>(1,0) = p1.getPoint(1).y;
+        P2.at<double>(2,0) = 1;
+
+        Mat P3 = Mat(3,1,CV_64F);
+        P3.at<double>(0,0) = p1.getPoint(2).x;
+        P3.at<double>(1,0) = p1.getPoint(2).y;
+        P3.at<double>(2,0) = 1;
+
+        Mat P4 = Mat(3,1,CV_64F);
+        P4.at<double>(0,0) = p1.getPoint(3).x;
+        P4.at<double>(1,0) = p1.getPoint(3).y;
+        P4.at<double>(2,0) = 1;
+
+        Mat P1New = H*P1;
+        Mat P2New = H*P2;
+        Mat P3New = H*P3;
+        Mat P4New = H*P4;
+
+        /*Mat X = Mat(4,1, CV_64F);
+        X.at<double>(0,0) = P1New.at<double>(0,0);
+        X.at<double>(1,0) = P2New.at<double>(0,0);
+        X.at<double>(2,0) = P3New.at<double>(0,0);
+        X.at<double>(3,0) = P4New.at<double>(0,0);
+
+        Mat Y = Mat(4,1, CV_64F);
+        Y.at<double>(0,0) = P1New.at<double>(1,0);
+        Y.at<double>(1,0) = P2New.at<double>(1,0);
+        Y.at<double>(2,0) = P3New.at<double>(1,0);
+        Y.at<double>(3,0) = P4New.at<double>(1,0);
+
+        double xMin = 9999;
+
+        for (int i=0; i<4; i++)
+        {
+            if (X.at<double>(i,0) < xMin)
+                xMin = X.at<double>(i,0);
+        }
+
+        double yMin = 9999;
+
+        for (int i=0; i<4; i++)
+        {
+            if (Y.at<double>(i,0) < yMin)
+                yMin = Y.at<double>(i,0);
+        }*/
+
+        Mat pts;
+        perspectiveTransform(src,pts, H);
+
+        double xMin = 9999;
+
+        for (int i=0; i<8; i+=2)
+        {
+            //cout << pts.at<double>(i,0) << endl;
+            if (pts.at<double>(0,i) < xMin)
+                xMin = pts.at<double>(0,i);
+        }
+
+        double yMin = 9999;
+
+        for (int i=1; i<8; i+=2)
+        {
+            //cout << pts.at<double>(0,i) << endl;
+            if (pts.at<double>(0,i) < yMin)
+                yMin = pts.at<double>(0,i);
+        }
+
+        H.at<double>(0,2) -= xMin;
+        H.at<double>(1,2) -= P3New.at<double>(1,0);
+
+        //cout << X << endl;
+        cout << "----------" << endl;
+        cout << pts << endl;
+
+        homographies.push_back(H);
 
     }
 
@@ -65,7 +147,7 @@ vector<Mat> HomographyCalculator::applyTransformation(Mat image){
 
     for (int i=0; i<nbHomographies; i++)
     {
-        warpPerspective(image, transformedImages.at(i), homographies.at(i), Size(image.cols, image.rows));
+        warpPerspective(image, transformedImages.at(i), homographies.at(i), Size(500, 500), WARP_INVERSE_MAP);
 
     }
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -7,62 +7,41 @@
 
 int main(int argc, char *argv[]) {
 
-    // Distorsions correction.
+    //sift = cv2.SIFT();
+    /// Distorsions correction.
     //Define the planes. The first one represents the distorted image.
     //The second one represents the points where the first plane should sit
-    Plane p1(Point2f(302,110), Point2f(310,294), Point2f(500,139), Point2f(501,309));
-    Plane p2(Point2f(500,139), Point2f(501,309), Point2f(617,122), Point2f(610,334));
-    Plane p3(Point2f(300,100), Point2f(300,522), Point2f(511,100), Point2f(511,522));
-    Plane p4(Point2f(511,100), Point2f(511,522), Point2f(722,100), Point2f(722,522));
+    Plane p1(Point2f(0,0), Point2f(0,422), Point2f(422,0), Point2f(422,422));
+    Plane p2(Point2f(306,24), Point2f(306,515), Point2f(610,84), Point2f(610,455));
 
     //Put the planes in vectors
     //The homography calculator takes plane vectors
     //in order to apply different transformations to different regions
     vector<Plane> a;
     a.push_back(p1);
-    a.push_back(p2);
 
     vector<Plane> b;
-    b.push_back(p3);
-    b.push_back(p4);
+    b.push_back(p2);
 
     //Calculate the homography
     HomographyCalculator hc;
     hc.determineHomographies(a, b);
 
     //Load the image
-    const char* nom1 = "../src/projection01.png";
+    const char* nom1 = "../src/grid-straight.png";
     Mat img = imread(nom1, CV_LOAD_IMAGE_GRAYSCALE);
     if (!img.data) {
         std::cout << " --(!) Error reading image" << std::endl;
         return -1;
     }
 
-    //Get the transformed image
+    // Image Warping
     vector<Mat> transformedImages = hc.applyTransformation(img);
 
-    CvRect roi1 = cvRect(300, 100, 211, 422);
-    CvRect roi2 = cvRect(511, 100, 211, 422);
-
-    Mat transformedLeft = Mat(transformedImages.at(0), roi1);
-    Mat transformedRight = Mat(transformedImages.at(1), roi2);
-
-    // Combine views
-    Mat combine = Mat(max(transformedLeft.rows, transformedRight.rows), transformedLeft.cols + transformedRight.cols, CV_8UC1);
-
-    CvRect left = cvRect(0, 0, transformedLeft.cols, transformedLeft.rows);
-    CvRect right = cvRect(transformedLeft.cols, 0, transformedRight.cols, transformedRight.rows);
-
-    Mat leftCombine = Mat(combine, left);
-    Mat rightCombine = Mat(combine, right);
-
-    transformedLeft.copyTo(leftCombine);
-    transformedRight.copyTo(rightCombine);
-
     //And show it
-    imshow("Transformed image", combine);
-
+    imshow("Transformed image", transformedImages.at(0));
     waitKey(0);
+
     /*/// Camera calibration.
     Calibration *c = new Calibration();
 
