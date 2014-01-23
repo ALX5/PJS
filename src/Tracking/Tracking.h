3f78dd59dff228471ae0148b1b8759e038dcbef4
bruno <bruno@superciberbruno.(none)>
2014-01-23 15:34:57 +0100

    Much faster alpha channel computation

--- src/Tracking/Tracking.h
+++ src/Tracking/Tracking.h
@@ -1,8 +1,8 @@
-//#include "../Main.h"
-//
-//class Tracking
-//{
-//public :
-//    Tracking();
-//    int setupTracking (int argc, char *argv[]);
-//};
+#include "../Main.h"
+
+class Tracking
+{
+public :
+    Tracking();
+    int setupTracking (int argc, char *argv[]);
+};
